6a6ec3f34bc677fe6d27afb39914192d12d58afe
Alexander Nähring <alexander.naehring.91@gmail.com>
2018-01-04 00:52:01 +0100

    dummy content for tabs

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -9,10 +9,24 @@ window.$ = window.jQuery = require('jQuery');
 // called when document is ready (fully loaded)
 $(document).ready(() => {
 
+  /* Define event handler */
+
   // nav-bar
   $('.nav-bar a').click( function () {
     // make tab active (li)
     $('.nav-bar li').removeClass('active');
     $(this).parent().addClass('active');
+
+    // show content
+    $('.content > section:visible').hide();
+    $($(this).attr('href')).show();
   });
+
+
+  /* Setup routine */
+
+  // start: activate mods tab
+  $('.nav-bar a[href="#mods"]').click();
+
+
 });
